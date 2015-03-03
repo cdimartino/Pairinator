f8c42676bd05ceadb882df515494dd5f71f0a714
Chris DiMartino <vwchris@gmail.com>
2015-03-03 17:07:11 -0500

    Adding exclusion list

--- pairinator.rb
+++ pairinator.rb
@@ -1,12 +1,18 @@
 require 'json'
 
 COHORT_FILE = ARGV[0]
+DONT_PAIR_FILE = ARGV[1]
+
 USERS = JSON.parse(File.read(COHORT_FILE))
+DONT_PAIR = JSON.parse(File.read(DONT_PAIR_FILE)) if DONT_PAIR_FILE
 
 def make_pair pairs
   user = pairs.keys.shuffle.first
   previous_pairs = pairs.delete(user)
   available_pairs = pairs.keys - previous_pairs
+  if DONT_PAIR.include? user
+    available_pairs -= DONT_PAIR
+  end
 
   if available_pairs.empty?
     available_pairs = pairs.keys
